8699ea94750e81f4a25cd3401e454dcf28826b07
AmolGitHubWorld <89722638+AmolGitHubWorld@users.noreply.github.com>
2022-07-14 09:05:33 +0530

    Add files via upload

---
+++ Assignment/12-07/display2/display2.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {Data1Service} from '../employees/data1.service';
+@Component({
+  selector: 'app-display2',
+  templateUrl: './display2.component.html',
+  styleUrls: ['./display2.component.css']
+})
+export class Display2Component implements OnInit {
+  Result:string="";
+  constructor(private obj : Data1Service) { }
+
+  ngOnInit(): void {
+    this.Result = this.obj.DisplayEmp();
+    
+  }
+
+}
